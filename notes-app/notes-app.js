2cc3b1e2f2d616af469ff34abb2477ace68ee52e
Adrian Gomez <apgomez85@gmail.com>
2018-11-20 20:50:59 -0800

    todo filter

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,14 +1,49 @@
 // DOM = Document Object Model
+const notes = [
+  {
+    title: 'My next trip',
+    body: 'I would like to got to Japan'
+  },
+  {
+    title: 'Habbits to work on',
+    body: 'Study more frequently'
+  },
+  {
+    title: 'Office modification',
+    body: 'Get a new seat'
+  }
+];
 
-// Query and remove
-// const p = document.querySelector('p');
-// p.remove();
+const filters = {
+  searchText: ''
+};
 
-// Query all and remove
-const ps = document.querySelectorAll('p');
+const renderNotes = function(notes, filters) {
+  const filteredNotes = notes.filter(function(note) {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
 
-ps.forEach(function(p) {
-  p.textContent = '******';
-  //   console.log(p.textContent);
-  //   p.remove();
+  document.querySelector('#notes').innerHTML = '';
+  filteredNotes.forEach(function(note) {
+    const noteEl = document.createElement('p');
+    noteEl.textContent = note.title;
+    document.querySelector('#notes').appendChild(noteEl);
+  });
+};
+
+renderNotes(notes, filters);
+
+document.querySelector('#create-note').addEventListener('click', function(e) {
+  e.target.textContent = 'The button was clicked';
+});
+
+document.querySelector('#remove-all').addEventListener('click', function() {
+  document.querySelectorAll('.note').forEach(function(note) {
+    note.remove();
+  });
+});
+
+document.querySelector('#search-text').addEventListener('input', function(e) {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
 });
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,70 @@
-const ps = document.querySelectorAll('p');
-
-ps.forEach(function(p) {
-  if (p.textContent.includes('the')) {
-    p.remove();
+const todos = [
+  {
+    text: 'Learn to code',
+    completed: false
+  },
+  {
+    text: 'Exercise',
+    completed: true
+  },
+  {
+    text: 'Eat Well',
+    completed: true
+  },
+  {
+    text: 'Save money',
+    completed: true
+  },
+  {
+    text: 'Clean up',
+    completed: false
   }
+];
+
+const filters = {
+  searchText: ''
+};
+
+const renderTodos = function(todos, filters) {
+  const filteredTodos = todos.filter(function(todo) {
+    return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+
+  let count = 0;
+  filteredTodos.forEach(todo => {
+    if (!todo.completed) {
+      count++;
+    }
+  });
+
+  document.querySelector('#todo-list').innerHTML = '';
+
+  const newParagraph = document.createElement('h2');
+  newParagraph.textContent = `You have ${count} todos left.`;
+  document.querySelector('#todo-list').appendChild(newParagraph);
+
+  filteredTodos.forEach(function(todo) {
+    const todoEl = document.createElement('p');
+    todoEl.textContent = todo.text;
+    document.querySelector('#todo-list').appendChild(todoEl);
+  });
+};
+
+todos.forEach(todo => {
+  const newParagraph = document.createElement('p');
+  newParagraph.textContent = todo.text;
+  document.querySelector('#todo-list').appendChild(newParagraph);
+});
+
+document.querySelector('#addButton').addEventListener('click', function() {
+  console.log("I'm adding a new todo.");
+});
+
+document.querySelector('#new-todo-text').addEventListener('input', function(e) {
+  console.log(e.target.value);
+});
+
+document.querySelector('#search-todo').addEventListener('input', function(e) {
+  filters.searchText = e.target.value;
+  renderTodos(todos, filters);
 });
